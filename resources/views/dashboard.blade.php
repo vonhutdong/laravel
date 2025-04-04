a868c7a4ee0620220ca00b8978c17616ac629c89
vonhutdong <162559123+vonhutdong@users.noreply.github.com>
2025-04-04 15:45:37 +0700

    c

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,10 @@
 <body>
 <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
     <div class="container">
-        <a class="navbar-brand mr-auto" href="#">Laravel Training</a>
+        <a class="navbar-brand mr-auto" href="#">CRUD USER</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
